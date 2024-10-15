eabae26a95da2f593df7c876084d923e0ae3d383
Andrei Guzei <aaguzey@gmail.com>
2024-10-15 11:49:11 +1000

    Initial commit

---
+++ src/test/java/com/fallt/hotel_reservation/HotelReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fallt.hotel_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
